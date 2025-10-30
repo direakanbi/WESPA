96ab542acd376ef9d29a8f27822122984a11009a
samuel-samson06 <a.samuelsamson@gmail.com>
2025-10-30 22:42:55 +0100

    update:created the Calendar page

--- components/tournaments/Calendar.tsx
+++ components/tournaments/Calendar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import DarkHero from '../DarkHero'
+import {Search} from "lucide-react";
+import { Input } from '../ui/input';
 // import {events} from "../../lib/dummyData" 
 
 const links = [
@@ -12,6 +14,14 @@ function Calendar() {
     <div>
         <DarkHero heading=" Tournament Calendar" text='Explore all WESPA-sanctioned tournaments by continent and category' links={links}/>
         
+        <div className=" p-10  flex flex-col items-center justify-center">
+          <section className="  flex items-center justify-center">
+            <Input type="text" placeholder="Search tournaments..." className=" py-4 px-5 w-[500px] bg-cyan-50 rounded-full border shadow-none  "/>
+              <Search className=' rounded-full p-1 -translate-x-8 bg-[#47b2e4] ' size={35} color={"#FFFFFF"}/>
+            {/* <div className=" bg-primary rounded-full">
+              </div> */}
+          </section>
+        </div>
     </div>
   )
 }
